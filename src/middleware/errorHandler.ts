ae62a027fb34a6569293aefc12766b2a4d169c3f
Gustavo Sanchez <gustavosanchezgalarza@gmail.com>
2025-05-02 16:53:53 -0500

    Fix error handler. Missing "next" parameter

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -1,12 +1,14 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 
 export interface IAppError extends Error {
   status?: number;
 }
 
-export const errorHandler = (err: IAppError, req: Request, res: Response) => {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const errorHandler = (err: IAppError, req: Request, res: Response, next: NextFunction) => {
   console.log(err);
   res.status(err.status ?? 500).json({
     message: err.message || "Internal Server Error",
+    stack: err.stack?.split("\n"),
   });
 };
