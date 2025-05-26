657b3afc18b3bf61c6958a378f1a441df19f54ae
Gustavo Sanchez <gustavosanchezgalarza@gmail.com>
2025-05-26 17:26:39 -0500

    Remove console.logs

--- src/utils/yearlyPeriodsCalculator.ts
+++ src/utils/yearlyPeriodsCalculator.ts
@@ -1,21 +1,18 @@
 import * as dateHandler from "typescript-calendar-date";
 
-export function getAnnualPeriods(start: string, end: string):dateHandler.CalendarDate[] {
+export function getAnnualPeriods(start: string, end: string): dateHandler.CalendarDate[] {
   const startDate: dateHandler.CalendarDate = dateHandler.parseIso8601String(start);
   const endDate: dateHandler.CalendarDate = dateHandler.parseIso8601String(end);
 
   const monthlyPeriods = dateHandler.periodOfMonths(startDate, endDate);
-  console.log("Monthly periods", monthlyPeriods);
-  const annualPeriods = monthlyPeriods.map((period) => (dateHandler.lastDateInMonth(period)))
-  console.log("Annual periods", annualPeriods); 
+  const annualPeriods = monthlyPeriods.map((period) => dateHandler.lastDateInMonth(period));
   const uniqueAnnualPeriods: dateHandler.CalendarDate[] = skipDuplicates(annualPeriods.reverse(), "year");
-  console.log("Unique annual periods", uniqueAnnualPeriods);
   return uniqueAnnualPeriods;
 }
 
-function skipDuplicates(arr: dateHandler.CalendarDate[], property: string): dateHandler.CalendarDate[]  {
+function skipDuplicates(arr: dateHandler.CalendarDate[], property: string): dateHandler.CalendarDate[] {
   const seenValues = new Set();
-  return arr.filter(obj  => {
+  return arr.filter((obj) => {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     const value: number = obj[property];
     if (seenValues.has(value)) {
@@ -25,4 +22,3 @@ function skipDuplicates(arr: dateHandler.CalendarDate[], property: string): date
     return true;
   });
 }
-
